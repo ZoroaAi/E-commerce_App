7048ac903ce221f52fe4f3ea8d2f78d2bc6cb518
ZoroAi <sauravkc456@gmail.com>
2023-08-27 16:59:11 +0100

    setted up tailwind

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <section className='text-3xl font-bold'>
+        Hello
+      </section>
+    </div>
+  );
+}
+
+export default App;
